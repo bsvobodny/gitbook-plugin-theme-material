fa13f37c476dd9f428118f3ceb659aabe135e1da
Benjamin Svobodny <svobodnb@int.videotron.com>
2017-06-21 10:01:17 -0400

    remove postInstall

--- src/build.sh
+++ src/build.sh
@@ -1,37 +1,45 @@
 #! /bin/bash
 pushd $(dirname "${0}") > /dev/null
 DIR=$(pwd -L)
+echo $DIR
 popd > /dev/null
 
 # Cleanup folder
+echo Cleanup folder
 rm -rf $DIR/../_assets
 
 # Recreate folder
+echo Recreate folder
 mkdir -p $DIR/../_assets/website/
 mkdir -p $DIR/../_assets/ebook/
 
 # Compile JS
+echo Compile JS
 browserify $DIR/js/core/index.js  | uglifyjs -mc > $DIR/../_assets/website/gitbook.js
 browserify $DIR/js/theme/index.js | uglifyjs -mc > $DIR/../_assets/website/theme.js
 # echo '\n/*Willin*/' >> $DIR/../_assets/website/gitbook.js
 # uglifyjs $DIR/../node_modules/materialize-css/bin/materialize.js >> $DIR/../_assets/website/gitbook.js
 
 # Compile Website CSS
+echo Compile Website CSS
 # lessc -clean-css less/website.less _assets/website/style.css
 lessc -clean-css $DIR/material/css/main.less $DIR/../_assets/website/style.css
 
 # Compile eBook CSS
+echo Compile eBook CSS
 lessc -clean-css $DIR/less/ebook.less $DIR/../_assets/ebook/ebook.css
 lessc -clean-css $DIR/less/pdf.less $DIR/../_assets/ebook/pdf.css
 lessc -clean-css $DIR/less/mobi.less $DIR/../_assets/ebook/mobi.css
 lessc -clean-css $DIR/less/epub.less $DIR/../_assets/ebook/epub.css
 
 # Copy fonts
+echo Copy fonts
 mkdir -p $DIR/../_assets/website/fonts
 cp -R $DIR/material/fonts/ $DIR/../_assets/website/fonts/
 cp -R $DIR/../node_modules/materialize-css/fonts/ $DIR/../_assets/website/fonts/
 
 # Copy icons
+echo Copy icons
 mkdir -p $DIR/../_assets/website/images
 cp $DIR/../node_modules/gitbook-logos/output/favicon.ico $DIR/../_assets/website/images/
 cp $DIR/../node_modules/gitbook-logos/output/apple-touch-icon-152.png $DIR/../_assets/website/images/apple-touch-icon-precomposed-152.png
--- src/js/core/index.js
+++ src/js/core/index.js
@@ -1,66 +1,64 @@
-// var $ = require('jquery');
-var $ = require('materialize-css/node_modules/jquery/dist/jquery.js');
+ var $ = require('jquery');
+ //var $ = require('materialize-css/node_modules/jquery/dist/jquery.js');
 
-var events  = require('./events');
-var storage = require('./storage');
-var page = require('./page');
+ var events = require('./events');
+ var storage = require('./storage');
+ var page = require('./page');
 
-var isPageReady = false;
-var onLoad = window.gitbook || [];
+ var isPageReady = false;
+ var onLoad = window.gitbook || [];
 
-// Export APIs for plugins
-var gitbook = {
-    events:   events,
-    page:     page,
+ // Export APIs for plugins
+ var gitbook = {
+     events: events,
+     page: page,
 
-    // Deprecated
-    state:    page.getState(),
+     // Deprecated
+     state: page.getState(),
 
-    // Read/Write the localstorage
-    storage: storage,
+     // Read/Write the localstorage
+     storage: storage,
 
-    // Push a function to be called once gitbook is ready
-    push: function(fn) {
-        if (!isPageReady) onLoad.push(fn);
-        else fn();
-    }
-};
-
-
-// Modules mapping for plugins
-var MODULES = {
-    'gitbook': gitbook,
-    'jquery':  $
-};
-
-window.gitbook = gitbook;
-window.$ = $;
-window.jQuery = $;
-
-require('materialize-css/bin/materialize.js');
-
-window.require = function (mods, fn) {
-    mods = mods.map(function(mod) {
-        mod = mod.toLowerCase();
-        if (!MODULES[mod]) {
-            throw new Error('GitBook module '+mod+' doesn\'t exist');
-        }
-
-        return MODULES[mod];
-    });
-
-    fn.apply(null, mods);
-};
-
-$(document).ready(function() {
-    isPageReady = true;
-    
-    $('.button-collapse').sideNav();
-    $('.active').parent('.collapsible-body').show().parent('li').addClass('active');
-    // Call pile of function once GitBook is ready
-    $.each(onLoad, function(i, fn) {
-        fn();
-    });
-});
+     // Push a function to be called once gitbook is ready
+     push: function (fn) {
+         if (!isPageReady) onLoad.push(fn);
+         else fn();
+     }
+ };
 
 
+ // Modules mapping for plugins
+ var MODULES = {
+     'gitbook': gitbook,
+     'jquery': $
+ };
+
+ window.gitbook = gitbook;
+ window.$ = $;
+ window.jQuery = $;
+
+ require('materialize-css/bin/materialize.js');
+
+ window.require = function (mods, fn) {
+     mods = mods.map(function (mod) {
+         mod = mod.toLowerCase();
+         if (!MODULES[mod]) {
+             throw new Error('GitBook module ' + mod + ' doesn\'t exist');
+         }
+
+         return MODULES[mod];
+     });
+
+     fn.apply(null, mods);
+ };
+
+ $(document).ready(function () {
+     isPageReady = true;
+
+     $('.button-collapse').sideNav();
+     $('.active').parent('.collapsible-body').show().parent('li').addClass('active');
+     // Call pile of function once GitBook is ready
+     $.each(onLoad, function (i, fn) {
+         fn();
+     });
+ });
